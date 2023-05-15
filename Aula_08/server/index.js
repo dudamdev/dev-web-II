bb5aaa938ec3a91299aa17237f4d66fc670c4d5c
dudamdev <duda.mdev@gmail.com>
2023-05-15 20:49:27 -0300

    Update index.js

--- Aula_08/server/index.js
+++ Aula_08/server/index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { pool } = require("./data/data");
-
 app.use(express.json());
 app.use(cors());
 app.listen(3000, () => {
-    console.log("Server running on port 3000!");
+    console.log("Servidor está ativo na porta 3000!");
 });
 
 process.on('SIGINT', () => {
@@ -19,18 +18,13 @@ let register = null;
 app.post("/api/registerUser", async (req, res) => {
     try {
         register = await pool.connect();
-        const { name, email, password } = req.body;
-        await register.query(`INSERT INTO Users (id, name, email, password) VALUES(uuid_generated_v4(), '${name}', '${email}', '${password}') `);
-        res.status(200).send("User successfully registered!");
-
+        const { nome, email, senha } = req.body;
+        await register.query(`INSERT INTO Users (id, nome, email, senha) VALUES (uuid_generate_v4(), '${nome}', '${email}', '${senha}')`);
+        res.status(200).send("Cadastro realizado com sucesso!");
     } catch (error) {
-        res.status(500).send("Error connecting with server");
+        console.error(error);
+        res.status(500).send("Não conectou ao servidor");
     } finally {
         register.release();
     }
-
 });
-
-
-
-
