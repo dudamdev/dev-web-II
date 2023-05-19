467c242986197a1bdfaf43afd2fe5ec2c13d88c2
dudamdev <duda.mdev@gmail.com>
2023-05-19 20:44:49 -0300

    Aula 19/05

---
+++ Aula_08/client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <RegisterUser></RegisterUser>
+</template>
+
+
+<script>
+import RegisterUser from './components/RegisterUser.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    RegisterUser
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Aula_08/client/src/components/RegisterUser.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+    <div>
+        <h1>Cadastro de Usuários</h1>
+        <form @submit.prevent="registroUsuario">
+            <div>
+                <label>Nome</label>
+                <input type="text" v-model="nome">
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="email" v-model="email">
+            </div>
+            <div>
+                <label>Senha</label>
+                <input type="password" v-model="senha">
+            </div>
+            <button type="submit">Registrar</button>
+        </form>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            nome:'',
+            email:'',
+            senha:'',
+            message:''
+        }
+    },
+    methods: {
+        registroUsuario() {
+            const data = {
+                nome: this.nome,
+                email: this.email,
+                senha: this.senha
+            }
+            console.log(data);
+        }
+    }
+}
+</script>
+<style lang="css">
+    
+</style>
